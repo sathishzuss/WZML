8a5f38b4bc4a0f79d30a3a777f8675727bfb6dca
deltech <sathishsoureche633@gmail.com>
2025-04-18 20:39:11 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 apt-get update && apt-get install -y git wget python3 python3-pip aria2 ffmpeg mediainfo unzip p7zip-full qbittorrent-nox
+unzip accounts.zip
 pip3 install -U -r requirements.txt
 python3 -m bot
